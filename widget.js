6222a9d029606da10f2593e087d8f0babc76e97d
Carberry <872072322@qq.com>
2018-12-29 16:43:04 +0800

    add 数组去重

--- widget.js
+++ widget.js
@@ -1,11 +1,11 @@
 // 相加函数，可接受任意数量的参数
 export function add(...args) {
-  return args.reduce((total, item) => total + item, 0);
+  return args.reduce((total, item) => total + item, 0)
 }
 //判断两个浮点数是否相等, range 为可接受的误差范围[-52，0],js能够表示的
 //最小精度就是 Number.EPSILON == 2 ** (-52);
 export function isFloatEqual(val1, val2, range = -52) {
-  return Math.abs(val1 - val2) < Number.EPSILON * Math.pow(2, 52 + range);
+  return Math.abs(val1 - val2) < Number.EPSILON * Math.pow(2, 52 + range)
 }
 
 /**
@@ -17,32 +17,32 @@ export function isFloatEqual(val1, val2, range = -52) {
  * @returns
  */
 export function flatten(arr, result) {
-  !result && (result = []);
+  !result && (result = [])
   for (let i = 0, j = arr.length; i < j; i++) {
     if (Array.isArray(arr[i])) {
-      flatten(arr[i], result);
+      flatten(arr[i], result)
     } else {
-      result.push(arr[i]);
+      result.push(arr[i])
     }
   }
-  return result;
+  return result
 }
 
 // 获取url的查询参数,就是?后面的一坨😜
 export const qs = name => {
-  let queryObj = {};
+  let queryObj = {}
   if (name && typeof name !== 'string')
-    throw new Error('the argument must be string');
+    throw new Error('the argument must be string')
   // 原生构造函数URLSearchParams，有兼容性
   if (URLSearchParams) {
-    const urlObj = new URLSearchParams(location.search);
+    const urlObj = new URLSearchParams(location.search)
     if (name) {
-      return urlObj.get(name);
+      return urlObj.get(name)
     }
     for (const item of urlObj.entries()) {
-      queryObj[item[0]] = item[1];
+      queryObj[item[0]] = item[1]
     }
-    return queryObj;
+    return queryObj
   } else {
     queryObj =
       window.location.search.length > 0
@@ -50,30 +50,30 @@ export const qs = name => {
             .substring(1)
             .split('&')
             .reduce((total, cur) => {
-              const temp = cur.split('=');
-              total[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
-              return total;
+              const temp = cur.split('=')
+              total[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1])
+              return total
             }, {})
-        : {};
+        : {}
     if (name) {
-      return queryObj[name];
+      return queryObj[name]
     }
-    return queryObj;
+    return queryObj
   }
-};
+}
 
 // 验证
 export function validate(value, type = 'require') {
   if (type === 'require') {
-    return !!value;
+    return !!value
   }
   if (type === 'mobile') {
-    return /^1[34578]\d{9}$/.test(value);
+    return /^1[34578]\d{9}$/.test(value)
   }
   if (type === 'email') {
     return /^[a-zA-Z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/.test(
       value
-    );
+    )
   }
 }
 
@@ -86,20 +86,20 @@ export function validate(value, type = 'require') {
  * @param {*} around 当前页前后出现的元素个数，默认两个
  */
 export function makePage(total, cur, around = 2) {
-  const baseCount = around * 2 + 1 + 2 + 2 + 2; // 元素总个数
-  const surplus = baseCount - 4; // 当省略号只有一个时。剩余元素的个数
-  const startPosition = 1 + 2 + around + 1; // 前面出现省略号的临界点
-  const endPosition = total - 2 - around - 1;
-  let result;
+  const baseCount = around * 2 + 1 + 2 + 2 + 2 // 元素总个数
+  const surplus = baseCount - 4 // 当省略号只有一个时。剩余元素的个数
+  const startPosition = 1 + 2 + around + 1 // 前面出现省略号的临界点
+  const endPosition = total - 2 - around - 1
+  let result
   if (cur <= baseCount - 2) {
-    result = Array.from({ length: baseCount - 2 }, (item, i) => i + 1);
+    result = Array.from({ length: baseCount - 2 }, (item, i) => i + 1)
   } else {
     if (cur < startPosition) {
       result = [
         ...Array.from({ length: surplus }, (item, i) => i + 1),
         '...',
         total,
-      ];
+      ]
     } else if (cur > endPosition) {
       result = [
         1,
@@ -108,7 +108,7 @@ export function makePage(total, cur, around = 2) {
           { length: surplus },
           (item, i) => total - surplus + i + 1
         ),
-      ];
+      ]
     } else {
       result = [
         1,
@@ -119,15 +119,15 @@ export function makePage(total, cur, around = 2) {
         ),
         '...',
         total,
-      ];
+      ]
     }
   }
-  return result;
+  return result
 }
 
 // 判断是否为普通对象
 export function isPlainObject(obj) {
-  return Object.prototype.toString.call(obj) === '[object Object]';
+  return Object.prototype.toString.call(obj) === '[object Object]'
 }
 /**
  *滚动进度条实现函数
@@ -146,13 +146,19 @@ export function isPlainObject(obj) {
      setProgress('.progress)
  */
 export function setProgress(selector) {
-  const wh = window.innerHeight;
-  const h = document.documentElement.scrollHeight;
-  const dh = h - wh;
+  const wh = window.innerHeight
+  const h = document.documentElement.scrollHeight
+  const dh = h - wh
   window.addEventListener('scroll', function() {
     requestAnimationFrame(() => {
-      const petcent = Math.max(0, Math.min(1, window.scrollY / dh));
-      document.querySelector(selector).style.width = petcent * 100 + '%';
-    });
-  });
+      const petcent = Math.max(0, Math.min(1, window.scrollY / dh))
+      document.querySelector(selector).style.width = petcent * 100 + '%'
+    })
+  })
 }
+// 数组去重
+export const deduplicate = arr =>
+  arr.reduce(
+    (total, cur) => (total.includes(cur) ? total : [...total, cur]),
+    []
+  )
