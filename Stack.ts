3714e0ad3a0a50483127e77a7d75136a70524586
CarberryChai <CarberryChai@gmail.com>
2020-08-01 09:43:39 +0800

    update

--- Stack.ts
+++ Stack.ts
@@ -1,16 +1,37 @@
 class Stack<T> {
   private stack_container: T[] = []
-  constructor() {}
-  isEmpty(): boolean {
-    return this.size() === 0
+  private n: number = 0
+  constructor(elements?: Iterable<T>) {
+    if (elements) {
+      for (const e of elements) {
+        this.push(e)
+      }
+    }
+  }
+  public isEmpty(): boolean {
+    return this.n === 0
   }
   push(item: T): void {
     this.stack_container.push(item)
+    this.n++
   }
-  pop(): T {
+  pop(): T | null | undefined {
+    if (this.isEmpty()) {
+      return null
+    }
+    this.n--
     return this.stack_container.pop()
   }
   size(): number {
-    return this.stack_container.length
+    return this.n
+  }
+
+  *[Symbol.iterator]() {
+    let i = this.n
+    while (i > 0) {
+      yield this.stack_container[--i]
+    }
   }
 }
+
+export default Stack
